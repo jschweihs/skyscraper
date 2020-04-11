ce35ae03599270f6012e2bedf8ccf986a3805293
J S <jschweihs@gmail.com>
2020-04-10 22:51:50 -0500

    Printing bytes received

--- src/netcomm.cpp
+++ src/netcomm.cpp
@@ -86,6 +86,7 @@ void NetComm::dataDownloaded(qint64 bytesReceived, qint64)
 {
   if(bytesReceived > MAXSIZE) {
     printf("Retrieved data size exceeded maximum of 42 MB, cancelling network request...\n");
+    printf("Size received: %u", bytesReceived)
     reply->abort();
   }
 }
